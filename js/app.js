124326da44dfaf13a8fdd4eb0f8e1a4bac8cef8c
alejandrogomezdp <alejandrogomezdp@gmail.com>
2023-03-09 20:29:57 +0100

    Primero

---
+++ js/app.js
@@ -0,0 +1,80 @@
+/* let nombre = "Alejandro"; */
+/* console.log(nombre) */
+
+
+/* Declaración de variables */
+/* let edad = 25;
+let nombre = "Alejandro";
+let email = "alejandrogomezdp@gmail.com";
+let temperatura = 22.5
+let bootcampinfo = "Full Stack Web Development Edición 12";
+
+console.log("Hola mi nombre es: ", nombre);
+console.log("Mi edad es: ", edad);
+console.log("Escribir un email a: ", email);
+console.log("Temperatura actual: ", temperatura);
+console.log("Estoy estudiando el bootcamp:", bootcampinfo) */
+
+/* let num1 = 10;
+let num2 = 5;
+
+let suma= num1 + num2 //15
+let resta= num1 - num2
+let multi = num1 * num2
+let divide= num1 / num2  */
+
+/* console.log("suma: ", suma)
+console.log("resta: ", resta)
+console.log("multi: ", multi)
+console.log("divide: ", divide) */
+
+/* let mensaje = "Hello, " + "world";
+console.log(mensaje);
+let name = "John";
+let lastname = "Connor";
+let fullname = userName + " " + lastName;
+
+console.log(userName + " " + lastName); */
+/* let user = "Sarah"
+let password = "abcd"
+
+let dia = "Buenos dias"
+let noche = "Buenas tardes" */
+
+/* dt1 = new Date("October 13, 2014 11:11:00");
+dt2 = new Date("October 13, 2014 11:13:00");
+console.log(diff_minutes(dt1, dt2)); */
+/* let time = 7;
+
+if (time >= 6 && time<=12){
+    console.log("Buenos días");
+}else if (time>=13 && time<=20){
+    console.log("Buenas tardes");
+}else if (time>=21 && time<=24){
+    console.log("Buenas noches");
+}else if (time>=0 && time<=5){
+    console.log("Buenas noches");
+}else{
+    console.log("Por favor, introduzca una hora válida")
+}
+
+ */
+let puesto = "1"
+
+if (puesto == 1) {
+    console.log("Enero");
+}else if (puesto == 2) {
+     console.log("Silver medal");
+}else if (puesto == 3){
+    console.log("Bronze");
+}else {
+console.log("Try it again!");
+}
+
+D E F - Invierno
+M A M - Primavera
+J J A - Verano
+Resto Otoño
+
+Enero 
+Febrero
